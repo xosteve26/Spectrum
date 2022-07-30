63646e88320d1e6849d391c75a13124a4d443fd1
Stephen George <stephengeorge2640@gmail.com>
2022-07-30 19:21:38 +0530

    Addition of save-song route

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 
 router.post('/login',async(req,res)=>{
     const {email,password} = req.body
+    console.log("EMAIL",email,"PASSWORD",password)
     try{
         const db = await pool.connect()
         const result=await db.query('SELECT * FROM users WHERE email=$1', [email])
@@ -20,7 +21,7 @@ router.post('/login',async(req,res)=>{
                     const user = result.rows[0]
                     const validPassword = await bcrypt.compare(password, user.password);
                     if (validPassword) {
-                        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+                        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET,{expiresIn:'1m'})
                         res.json({
                             userId: user.id,
                             token: token
@@ -82,6 +83,33 @@ router.post('/register',async(req,res)=>{
     }
         }
     )
+
+router.post('/save-song',async(req,res)=>{
+    const {song_id,track_name,artist_name,userID, rating,genre} = req.body
+    console.log("song_id",song_id,"track_name",track_name,"artist_name",artist_name,"userID",userID,"rating",rating,"genre",genre)
+    if (!song_id || !track_name || !artist_name || !userID || !rating.toString() || !genre) {
+        return res.status(400).json({ error: "missing required fields" })
+    }
+
+    try{
+        const db=await pool.connect()
+        const queryText = 'INSERT INTO wishlist (id,userid,track_name,artist_name,genre,song_id, explicit) VALUES ($1,$2,$3,$4,$5,$6,$7)'
+        const values = [uuidv4(), userID,track_name, artist_name,genre, song_id, Boolean(rating) ]
+        const result=await db.query(queryText, values)
+            if (!result) {
+                return res.status(400).json({ error: "error creating song in database" })
+            }
+            else {
+                res.status(200).json({ message: "song created successfully" })
+            }
+        
+    }
+    catch(err){
+        console.log(err.message)
+    }
+        }
+    )
+
 
 
 module.exports=router
