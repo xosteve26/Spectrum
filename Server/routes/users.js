7cf4db33729a892b0f5d3eecc37dfc05a9c48962
Stephen George <stephengeorge2640@gmail.com>
2022-08-01 14:08:15 +0530

    Addition of fetch user route

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -13,7 +13,9 @@ router.post('/login',async(req,res)=>{
     console.log("EMAIL",email,"PASSWORD",password)
     try{
         const db = await pool.connect()
+        console.log("DB CONNECT")
         const result=await db.query('SELECT * FROM users WHERE email=$1', [email])
+        console.log("LOGIN RESULT",result.rows[0])
             if (!result) {
                 return res.status(400).json({ error: "error querying database" })
             }
@@ -42,6 +44,7 @@ router.post('/login',async(req,res)=>{
     
     catch(err){
         console.log(err.message)
+        pool.close()
     }
    
     }
@@ -132,5 +135,91 @@ router.get('/get-songs',authMiddleware,async(req,res)=>{
 
     }
 )
+
+
+router.get('/get-user',authMiddleware,async(req,res)=>{
+    const userID = req.userID
+
+    try{
+        const db=await pool.connect()
+        const result=await db.query('SELECT * FROM users WHERE id=$1',[userID])
+        console.log(result.rows)
+            if (!result) {
+                return res.status(400).json({ error: "error querying database" })
+            }
+            else {
+                res.status(200).json(result.rows)
+            }
+        
+    }
+    catch(err){
+        console.log(err.message)
+    }
+
+    }
+
+)
+
+
+router.put('/update-user',authMiddleware,async(req,res)=>{
+    console.log("UPDATE USER")
+    const {name,email,password} = req.body
+    console.log("UPDATE DATA", name,email,password)
+    const userID = req.userID
+    try{
+        const db=await pool.connect()
+        console.log("DB CONNECT")
+        const result=await db.query('SELECT * FROM users WHERE id=$1',[userID])
+        console.log(result.rows)
+            if (!result) {
+                return res.status(400).json({ error: "error querying database" })
+            }
+            else {
+                const user = result.rows[0]
+                if(password == null){
+                    console.log("IN IF")
+                    const queryText = 'UPDATE users SET name=$1,email=$2 WHERE id=$3'
+                    const values = [name, email, userID]
+                    const result=await db.query(queryText, values)
+                        if (!result) {
+                            return res.status(400).json({ error: "error updating user in database" })
+                        }
+                        else {
+                            res.status(200).json({ message: "user updated successfully" })
+                        }
+                }
+                else{
+                    console.log("IN ELSE")
+
+                    const salt = await bcrypt.genSalt(10);
+                    const hash_password = await bcrypt.hash(password, salt)
+                    if (hash_password) {
+                        const queryText = 'UPDATE users SET name=$1,email=$2,password=$3 WHERE id=$4'
+                        const values = [name, email, hash_password, userID]
+                        const result = await db.query(queryText, values)
+                        if (!result) {
+                            return res.status(400).json({ error: "error updating user in database" })
+                        }
+                        else {
+                            res.status(200).json({ message: "user updated successfully" })
+                        }
+
+                    }
+                    else {
+                        return res.status(400).json({ error: "incorrect password" })
+                    }
+
+                }
+                
+            }
+        
+    }
+    catch(err){
+        console.log(err.message)
+    }
+
+    
+})
+
 
 module.exports=router
