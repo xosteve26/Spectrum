76552d0cb52a34da38ea39bfcabb0c33f847032f
Stephen George <stephengeorge2640@gmail.com>
2022-08-05 21:36:11 +0530

    Fix for audio player

--- frontend/src/components/MediaPlayer.js
+++ frontend/src/components/MediaPlayer.js
@@ -3,16 +3,28 @@
 
 import ReactAudioPlayer from 'react-audio-player';
 
-const MediaPlayer = () => {
+
+const MediaPlayer = ({url}) => {
   return (
-    
-    
+
     <ReactAudioPlayer
-        src="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview112/v4/1b/aa/25/1baa2573-8d81-8daa-08dd-b68c6be010a0/mzaf_595184799298784750.plus.aac.ep.m4a"
+        src={url}
         controls
         preload='auto'
-    />
+        style={{"width": "100%"}}
 
+    />
+    // <>
+    //   <AudioPlayer
+    //     autoPlay
+    //     src="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview112/v4/1b/aa/25/1baa2573-8d81-8daa-08dd-b68c6be010a0/mzaf_595184799298784750.plus.aac.ep.m4a"
+    //     onPlay={e => console.log("onPlay")}
+
+    //   // other props here
+    //   />
+    // </>
+   
+   
   )
 }
 
