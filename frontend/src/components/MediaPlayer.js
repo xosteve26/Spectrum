8c2f0cc93a18795cc4f90b30ae1bdb5ed7dfa7dc
Stephen George <stephengeorge2640@gmail.com>
2022-08-05 21:36:38 +0530

    Updated fix for audio player

--- frontend/src/components/MediaPlayer.js
+++ frontend/src/components/MediaPlayer.js
@@ -1,6 +1,3 @@
-// import AudioPlayer from 'react-h5-audio-player';
-// import 'react-h5-audio-player/lib/styles.css';
-
 import ReactAudioPlayer from 'react-audio-player';
 
 
@@ -14,17 +11,7 @@ const MediaPlayer = ({url}) => {
         style={{"width": "100%"}}
 
     />
-    // <>
-    //   <AudioPlayer
-    //     autoPlay
-    //     src="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview112/v4/1b/aa/25/1baa2573-8d81-8daa-08dd-b68c6be010a0/mzaf_595184799298784750.plus.aac.ep.m4a"
-    //     onPlay={e => console.log("onPlay")}
 
-    //   // other props here
-    //   />
-    // </>
-   
-   
   )
 }
 
